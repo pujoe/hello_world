1438c55660cee75e39531bb519dc89f2111ac31e
Vivek <kxy@users.noreply.github.com>
2015-04-10 16:16:14 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(){
-    std::cout << "hello, world" << std::endl;
+    std::cout << "I think I am a little bit don\'t know how to write a c++ program" << std::endl;
     return 0;
 }
